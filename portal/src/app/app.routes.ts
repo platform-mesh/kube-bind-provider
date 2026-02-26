d6e776413fd1acecc16e5337d25d0c47d849f66e
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-02-26 16:06:14 +0200

    wire in requests

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { BindingRequestsComponent } from './binding-requests/binding-requests.component';
 import { ClusterBindingsComponent } from './cluster-bindings/cluster-bindings.component';
+import { ServiceMappingsComponent } from './service-mappings/service-mappings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'requests', pathMatch: 'full' },
   { path: 'requests', component: BindingRequestsComponent },
   { path: 'bindings', component: ClusterBindingsComponent },
+  { path: 'service-mappings', component: ServiceMappingsComponent },
   { path: '**', redirectTo: 'requests' },
 ];
